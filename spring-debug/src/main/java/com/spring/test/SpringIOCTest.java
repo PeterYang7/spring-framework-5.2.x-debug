bb6f7876a7cdfdaa6286ba591f14217b55cf3c74
peter <1042397946@qq.com>
2021-02-05 16:51:06 +0800

    自定义标签解析

--- spring-debug/src/main/java/com/spring/test/SpringIOCTest.java
+++ spring-debug/src/main/java/com/spring/test/SpringIOCTest.java
@@ -1,7 +1,7 @@
 package com.spring.test;
 
-import com.spring.extend.MyClassPathXmlApplicationContext;
-import com.spring.selftag.User;
+
+import com.spring.bean.User;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -11,16 +11,17 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
  */
 public class SpringIOCTest {
 	public static void main(String[] args) {
-		ApplicationContext applicationContext = new ClassPathXmlApplicationContext("ioc.xml");
-//		User user = (User)applicationContext.getBean("user");
-//		System.out.println(user);
+		ApplicationContext applicationContext = new ClassPathXmlApplicationContext("spring-${username}.xml");
+		User user = (User)applicationContext.getBean("user");
+		System.out.println(user);
+
 //		MyClassPathXmlApplicationContext context = new MyClassPathXmlApplicationContext("ioc.xml");
 
 //		ApplicationContext context = new ClassPathXmlApplicationContext("spring-${username}.xml");
 //		User user1 = (User) context.getBean("user");
 //		System.out.println(user1);
 
-		User user = (User) applicationContext.getBean("peter");
-		System.out.println(user.getUsername());
+//		User user = (User) applicationContext.getBean("peter");
+//		System.out.println(user.getUsername());
 	}
 }
